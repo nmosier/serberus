6dced36ee6f7fe52629fd77e1dfd4b57a5dc5e2b
Nicholas Mosier <nmosier@stanford.edu>
2022-09-08 16:11:21 -0700

    Implement No Spill Lowering pass

---
+++ src/NoSpillLowering.cc
@@ -0,0 +1,42 @@
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/InlineAsm.h>
+
+#include "util.h"
+#include "metadata.h"
+
+namespace clou {
+  namespace {
+    struct NoSpillLowering final : public llvm::FunctionPass {
+      static inline char ID = 0;
+      NoSpillLowering(): llvm::FunctionPass(ID) {}
+
+      static llvm::FunctionCallee getNoSpillIntrinsic(llvm::Module *M, llvm::Type *T) {
+	llvm::LLVMContext& ctx = M->getContext();
+	llvm::FunctionType *fty = llvm::FunctionType::get(llvm::Type::getVoidTy(ctx), {T}, false);
+	return llvm::InlineAsm::get(fty, "clou.nospill", "r", true);
+      }
+
+      bool runOnFunction(llvm::Function& F) override {
+	bool changed = false;
+	for (llvm::BasicBlock& B : F) {
+	  for (llvm::Instruction *I = &B.front(); I != nullptr; I = I->getNextNode()) {
+	    if (md::getMetadataFlag(I, md::nospill)) {
+	      // insert call
+	      llvm::IRBuilder<> IRB (I->getNextNode());
+	      IRB.CreateCall(getNoSpillIntrinsic(F.getParent(), I->getType()), {I});
+	      changed = true;
+	    }
+	  }
+	}
+	return changed;
+      }
+    };
+
+    llvm::RegisterPass<NoSpillLowering> X {"nospill-lowering", "Clou's 'nospill' Lowering Pass"};
+    util::RegisterClangPass<NoSpillLowering> Y;
+  }
+}
---
+++ src/metadata.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace clou {
+
+  inline const char speculative_inbounds[] = "specinbounds";
+  
+}
