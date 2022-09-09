6a1c3be283104100fe10a9a4e18ced9b2e9d9b77
Nicholas Mosier <nmosier@stanford.edu>
2022-09-09 11:17:27 -0700

    Add CommandLine library with shared Clou compile options

---
+++ src/CommandLine.cc
@@ -0,0 +1,7 @@
+#include "CommandLine.h"
+
+namespace clou {
+
+  llvm::cl::opt<std::string> emit_dot("emit-dot", llvm::cl::desc("Clou: emit dot graphs"));
+
+}
---
+++ src/CommandLine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include <llvm/Support/CommandLine.h>
+
+namespace clou {
+
+  extern llvm::cl::opt<std::string> emit_dot;
+  
+}
