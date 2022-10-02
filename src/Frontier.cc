96b45f8862e17348dbfe11af593729f3f3522070
Nicholas Mosier <nmosier@stanford.edu>
2022-10-02 16:28:16 +0000

    initial commit

---
+++ src/Frontier.cc
@@ -0,0 +1,69 @@
+#include "Frontier.h"
+
+#include <algorithm>
+
+#include "util.h"
+
+namespace clou {
+
+  ISet reachable_predecessors(llvm::Instruction *root) {
+    ISet seen;
+    IQueue todo;
+    todo.push(root);
+    while (!todo.empty()) {
+      auto *I = todo.front();
+      todo.pop();
+      if (seen.insert(I).second) {
+	for (auto *pred : llvm::predecessors(I)) {
+	  todo.push(pred);
+	}
+      }
+    }
+    return seen;
+  }
+
+  namespace {
+    template <class NextFunc>
+    bool frontier_impl(llvm::Instruction *endpoint, std::function<bool (llvm::Instruction *)> pred, ISet& frontier,
+		       NextFunc nexts) {
+      auto& F = *endpoint->getFunction();
+      const auto subgraph = reachable_predecessors(endpoint);
+      ISet in;
+      copy_if(F, std::inserter(in, in.end()), [&] (llvm::Instruction *I) -> bool {
+	return subgraph.contains(I) && pred(I);
+      });
+
+      ISet seen;
+      IQueue todo;
+      todo.push(&F.getEntryBlock().front());
+      bool result = true;
+      auto out = std::inserter(frontier, frontier.end());
+      while (!todo.empty()) {
+	auto *I = todo.front();
+	todo.pop();
+	if (seen.insert(I).second) {
+	  if (I == endpoint) {
+	    result = false;
+	  } else if (in.contains(I)) {
+	    *out++ = I;
+	  } else {
+	    for (auto *next : nexts(I)) {
+	      todo.push(next);
+	    }
+	  }
+	}
+      }
+
+      return result;
+    }
+  }
+
+  bool forward_frontier(llvm::Instruction *endpoint, std::function<bool (llvm::Instruction *)> pred, ISet& frontier) {
+    return frontier_impl(endpoint, pred, frontier, &llvm::successors_inst);
+  }
+
+  bool reverse_frontier(llvm::Instruction *endpoint, std::function<bool (llvm::Instruction *)> pred, ISet& frontier) {
+    return frontier_impl(endpoint, pred, frontier, [] (llvm::Instruction *I) { return llvm::predecessors(I); });
+  }
+  
+}
---
+++ src/Frontier.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <queue>
+#include <set>
+#include <functional>
+
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/ADT/iterator.h>
+#include <llvm/IR/InstIterator.h>
+
+namespace clou {
+
+  using ISet = std::set<llvm::Instruction *>;
+  using IQueue = std::queue<llvm::Instruction *>;
+
+  template <class OutputIt, class UnaryPredicate>
+  OutputIt copy_if(llvm::Function& F, OutputIt out, UnaryPredicate pred) {
+    using PointerIt = llvm::pointer_iterator<llvm::inst_iterator>;
+    return std::copy_if(PointerIt(llvm::inst_begin(F)), PointerIt(llvm::inst_end(F)), out, pred);
+  }
+
+  ISet reachable_predecessors(llvm::Instruction *root);
+  bool forward_frontier(llvm::Instruction *endpoint, std::function<bool (llvm::Instruction *)> pred, ISet& frontier);
+  bool reverse_frontier(llvm::Instruction *endpoint, std::function<bool (llvm::Instruction *)> pred, ISet& frontier);
+
+}
