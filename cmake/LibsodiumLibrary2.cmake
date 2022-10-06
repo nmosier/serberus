7bb0dc5b5c5b093dcdba9c078776c66813413f70
Nicholas Mosier <nmosier@stanford.edu>
2022-10-06 20:21:22 +0000

    Build libsodium differently, in a way that allows us to properly respect dependencies

--- cmake/LibsodiumLibrary2.cmake
+++ cmake/LibsodiumLibrary2.cmake
@@ -1,7 +1,7 @@
 set(MERGE_COUNTS_PY ${CMAKE_SOURCE_DIR}/scripts/merge_counts.py)
 set(SYMBOLIZE_SH ${CMAKE_SOURCE_DIR}/scripts/srclocs2.sh)
 
-function(libsdoium_library NAME)
+function(libsodium_library NAME)
   set(multi_value_args DEPENDS PASS CPPFLAGS CFLAGS LDFLAGS LLVMFLAGS CLOUFLAGS CONFIGURE_OPTIONS)
   cmake_parse_arguments(LIBSODIUM "" "" "${multi_value_args}" ${ARGN})
 
@@ -22,35 +22,70 @@ function(libsdoium_library NAME)
   endforeach()
 
   list(APPEND LIBSODIUM_LDFLAGS -pthread)
-  
-  list(APPEND LIBSODIUM_CFLAGS "-g")
+
   list(JOIN LIBSODIUM_CFLAGS " " LIBSODIUM_CFLAGS)
   list(JOIN LIBSODIUM_CPPFLAGS " " LIBSODIUM_CPPFLAGS)
   list(JOIN LIBSODIUM_LDFLAGS " " LIBSODIUM_LDFLAGS)
 
-  set(PREFIX_DIR ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
+  set(PREFIX_DIR ${CMAKE_CURRENT_BINARY_DIR}/${NAME})
   set(BUILD_DIR ${PREFIX_DIR}/build)
+  set(STAMP_DIR ${PREFIX_DIR}/stamp)
   set(INSTALL_DIR ${PREFIX_DIR})
-  set(STAMP_FILE ${PREFIX_DIR}/stamp)
 
+  make_directory(${INSTALL_DIR}/include)
+  make_directory(${BUILD_DIR})
+  
   # configure command
-  add_custom_command(OUTPUT ${BUILD_DIR}/Makefile
-    COMMAND ${LIBSODIUM_DIR}/configure --quiet --prefix=${CMAKE_CURRENT_BINARY_DIR}/${NAME}-prefix CC=${LLVM_BINARY_DIR}/bin/clang "CPPFLAGS=${LIBSODIUM_CPPFLAGS}" "CFLAGS=${LIBSODIUM_CFLAGS}" "LDFLAGS=${LIBSODIUM_LDFLAGS}" ${LIBSODIUM_CONFIGURE_OPTIONS}
-    DEPENDS ${LIBSODIUM_DEPENDS}
+  add_custom_command(OUTPUT ${STAMP_DIR}/configure.stamp
+    COMMAND ${LIBSODIUM_DIR}/configure --quiet --prefix=${PREFIX_DIR} CC=${LLVM_BINARY_DIR}/bin/clang LD=${LLVM_BINARY_DIR}/bin/ld.lld "CPPFLAGS=${LIBSODIUM_CPPFLAGS}" "CFLAGS=${LIBSODIUM_CFLAGS}" "LDFLAGS=${LIBSODIUM_LDFLAGS}" ${LIBSODIUM_CONFIGURE_OPTIONS}
+    COMMAND touch ${STAMP_DIR}/configure.stamp
     COMMENT "Configuring libsodium library ${NAME}"
     WORKING_DIRECTORY ${BUILD_DIR}
   )
 
   # clean step
-  add_custom_command(OUTPUT ${STAMP_FILE}
-    COMMAND make --quiet clean && touch ${STAMP_FILE}
-    DEPENDS ${BUILD_DIR}/Makefile
+  add_custom_command(OUTPUT ${STAMP_DIR}/clean.stamp
+    COMMAND make --quiet clean
+    COMMAND touch ${STAMP_DIR}/clean.stamp
+    DEPENDS ${STAMP_DIR}/configure.stamp
     COMMENT "Cleaning libsodium library ${NAME}"
     WORKING_DIRECTORY ${BUILD_DIR}
   )
 
   # build step
-  add_custom_command(OUTPUT ${BUILD_DIR}/src/libsodium/.libs/libsodium.a
-    COMMAND make -j64 && make check -j64
-    DEPENDS ${BUILD_DIR}
-    
+  add_custom_command(OUTPUT ${STAMP_DIR}/build.stamp
+    COMMAND make --quiet -j64
+    COMMAND touch ${STAMP_DIR}/build.stamp
+    DEPENDS ${STAMP_DIR}/clean.stamp
+    COMMENT "Building libsodium library ${NAME}"
+    WORKING_DIRECTORY ${BUILD_DIR}
+  )
+
+  # test step
+  add_custom_command(OUTPUT ${STAMP_DIR}/test.stamp
+    COMMAND make --quiet -j64 check
+    COMMAND touch ${STAMP_DIR}/test.stamp
+    DEPENDS ${STAMP_DIR}/build.stamp
+    COMMENT "Testing libsodium library ${NAME}"
+    WORKING_DIRECTORY ${BUILD_DIR}
+  )
+
+  # install step
+  add_custom_command(OUTPUT ${STAMP_DIR}/install.stamp ${INSTALL_DIR}/lib/libsodium.so ${INSTALL_DIR}/include/sodium.h
+    COMMAND make --quiet install
+    COMMAND touch ${STAMP_DIR}/install.stamp
+    DEPENDS ${STAMP_DIR}/test.stamp
+    COMMENT "Installing libsodium library ${NAME}"
+    WORKING_DIRECTORY ${BUILD_DIR}
+  )
+
+  add_custom_target(${NAME}_install ALL
+    DEPENDS ${STAMP_DIR}/install.stamp
+  )
+
+  add_library(${NAME} INTERFACE)
+  target_link_libraries(${NAME} INTERFACE ${INSTALL_DIR}/lib/libsodium.so)
+  target_include_directories(${NAME} INTERFACE ${INSTALL_DIR}/include)
+  add_dependencies(${NAME} ${NAME}_install)
+
+endfunction()
