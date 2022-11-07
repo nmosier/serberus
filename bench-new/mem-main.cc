894ebcb2015aaceb1353a7d8fb79ec25530bd1fe
Nicholas Mosier <nmosier@stanford.edu>
2022-11-07 22:42:40 +0000

    Accept --benchmark_out command line option

--- bench-new/mem-main.cc
+++ bench-new/mem-main.cc
@@ -5,23 +5,44 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
+#include <unistd.h>
+#include <sys/wait.h>
+#include <climits>
 #include <cstring>
+#include <vector>
+#include <fstream>
 
 static void print_memusage(std::ostream& os) {
   struct rusage ru;
-  if (getrusage(RUSAGE_SELF, &ru) < 0)
+  if (getrusage(RUSAGE_CHILDREN, &ru) < 0)
     err(EXIT_FAILURE, "getrusage");
   os << "{\"mem\": " << ru.ru_maxrss << ", \"unit\": \"KB\"}" << "\n";
 }
 
-int main() {
-  print_memusage(std::cout);
-  benchmark::State state(BENCH_ARG);
-  BENCH_NAME(state);
+int main(int argc, char *argv[]) {  
+  const pid_t pid = fork();
+  if (pid < 0) {
+    err(EXIT_FAILURE, "fork");
+  } else if (pid == 0) {
+    benchmark::State state(BENCH_ARG);
+    BENCH_NAME(state);
+  } else {
+    
+    if (wait(nullptr) < 0) {
+      err(EXIT_FAILURE, "wait");
+    }
 
-  std::vector<char> vec(1000000);
-  for (char &c : vec) c = rand();
-  
-  print_memusage(std::cout);
+    std::ofstream ofs;
+    std::ostream *os = &std::cout;
+    for (int i = 1; i < argc; ++i) {
+      const char *s = argv[i];
+      std::vector<char> path(std::strlen(s) + 1);
+      if (std::sscanf(argv[i], "--benchmark_out=%s", path.data()) == 1) {
+	ofs.open(path.data());
+	os = &ofs;
+      }
+    }    
+    print_memusage(*os);
+    
+  }
 }
