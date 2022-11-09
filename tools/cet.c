0ce20754778c3bf7064a5af17387749d913dae45
Nicholas Mosier <nmosier@stanford.edu>
2022-11-09 16:47:29 +0000

    explicitly declare arch_prctl

--- tools/cet.c
+++ tools/cet.c
@@ -1,4 +1,3 @@
-#include <sys/prctl.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -7,17 +6,19 @@
 
 #define ARCH_X86_CET_STATUS                0x3001
 
+extern int arch_prctl(int code, long *addr);
+
 __attribute__((constructor)) void require_ibt(void) {
   if (getenv("NOCET")) {
     return;
   }
   long buf[3];
   if (arch_prctl(ARCH_X86_CET_STATUS, buf) < 0) {
-    fprintf(stderr, "%s: arch_prctl: %s\n", __FUNCTION__, strerror(errno));
+    fprintf(stderr, "%s: arch_prctl: %s\n", __func__, strerror(errno));
     _Exit(EXIT_FAILURE);
   }
   if ((buf[0] & 1) == 0) {
-    fprintf(stderr, "%s: IBT not enabled\n", __FUNCTION__);
+    fprintf(stderr, "%s: IBT not enabled\n", __func__);
     _Exit(EXIT_FAILURE);
   }
 }
