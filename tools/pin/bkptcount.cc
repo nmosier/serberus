01e611801fa89b8e4cab3dc661c615e71392009c
Nicholas Mosier <nmosier@stanford.edu>
2022-11-29 22:17:24 +0000

    count mitigations

---
+++ tools/pin/bkptcount.cc
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2004-2021 Intel Corporation.
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <iostream>
+#include <fstream>
+#include "pin.H"
+using std::cerr;
+using std::endl;
+using std::ios;
+using std::ofstream;
+using std::string;
+
+ofstream OutFile;
+
+static UINT64 sentinels_seen = 0;
+
+static VOID handle_sentinel() {
+  ++sentinels_seen;
+}
+
+// Pin calls this function every time a new instruction is encountered
+VOID Instruction(INS ins, VOID* v) {
+  if (INS_Opcode(ins) == XED_ICLASS_MFENCE) {
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) handle_sentinel, IARG_END);
+    INS_Delete(ins);
+  }
+}
+
+KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "inscount.out", "specify output file name");
+
+// This function is called when the application exits
+VOID Fini(INT32 code, VOID* v) {
+    std::cout << "Count: " << sentinels_seen << "\n";
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
+INT32 Usage()
+{
+    cerr << "This tool counts the number of breakpoints executed" << endl;
+    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
+    return -1;
+}
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
+/*   argc, argv are the entire command line: pin -t <toolname> -- ...    */
+/* ===================================================================== */
+
+int main(int argc, char* argv[])
+{
+    // Initialize pin
+    if (PIN_Init(argc, argv)) return Usage();
+
+    OutFile.open(KnobOutputFile.Value().c_str());
+
+    // Register Instruction to be called to instrument instructions
+    INS_AddInstrumentFunction(Instruction, 0);
+
+    // Register Fini to be called when the application exits
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Start the program, never returns
+    PIN_StartProgram();
+
+    return 0;
+}
