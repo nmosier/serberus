3cdf657234a13421b5a5b6ba84e5239abc40b8b4
Nicholas Mosier <nmosier@stanford.edu>
2023-02-26 10:33:59 -0800

    Rename bench-new to bench

--- bench/plot.py
+++ bench/plot.py
@@ -1,107 +1,114 @@
 import argparse
-import sys
-import os
-import json
-import tempfile
-import math
-import matplotlib.pyplot as plt
-import seaborn
 import shutil
-import collections
+import os
+from collections import defaultdict
+import json
 import pandas
+import seaborn
+import matplotlib.pyplot as plt
+import math
 
 parser = argparse.ArgumentParser()
-parser.add_argument('spec')
-parser.add_argument('-o', dest = 'outdir', required = True, action = 'store')
-parser.add_argument('-a', dest = 'run_all', action = 'store_true')
-parser.add_argument('-B', dest = 'remake_all', action = 'store_true')
+parser.add_argument('json', nargs = '+')
+parser.add_argument('-o', dest = 'out')
 args = parser.parse_args()
 
-with open(args.spec) as f:
-    spec = json.load(f)
+def get_basename(path):
+    return os.path.splitext(os.path.basename(path))[0]
 
-if args.remake_all:
-    shutil.rmtree(args.outdir)
+# Infer the metric from the filenames
 
-os.makedirs(args.outdir, exist_ok = True)
-    
-benchmark_flags = spec['benchmark_flags']
-def run_benchmark(exe, outpath):
-    args = [exe, f'--benchmark_out={outpath}', '--benchmark_out_format=json', *benchmark_flags]
-    cmd = ' '.join(args)
-    print(f'Running: {cmd}', file = sys.stderr)
-    if os.system(' '.join(args)) != 0:
-        print('benchmark failed: ', args, file = sys.stderr)
-        exit(1)
+metric = get_basename(args.json[0]).split('_', maxsplit = 1)[0]
+metric_keys = {'time': 'cpu_time'}
+metric_key = metric_keys[metric] if metric in metric_keys else metric
+
+# From the JSON filenames, we can gather the exact benchmarks.
+# time_<lib>_<size>_<mode>.json
+
+# function_displaynames = {
+#     "salsa20": "Salsa20",
+#     "sha256": "SHA-256",
+#     "chacha20": "ChaCha20",
+#     "poly1305": "Poly1305",
+#     "curve25519": "Curve25519",
+# }
 
 
-def maybe_run_benchmark(exe, outpath):
-    if args.run_all or not os.access(outpath, os.R_OK):
-        run_benchmark(exe, outpath)
-        assert os.access(outpath, os.R_OK)
+# The structure needs to looks like the following.
+# | <name>_<size> | overhead | mitigation |
 
-# Required format:
-# x = benchmark name
-# y = overhead
-# hue = mitigation
-# errorbar = sd
+# map from run (<lib>_<name>_<size>) to list of baseline types
+benchmarks = defaultdict(dict)
+for jsonpath in args.json:
+    benchtype, lib, name, size, mode = os.path.basename(os.path.splitext(jsonpath)[0]).split('_', maxsplit = 4)
 
-# benches: benchname -> mitname -> recordname -> values
-benches = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(list)));
+    benchmark = (lib, name, size)
+    with open(jsonpath, 'r') as f:
+        j = json.load(f)
+    # strip away unneeded information in json
+    j_ = None
+    for result in j['benchmarks']:
+        if result['name'] == f'{lib}_{name}/{size}_mean':
+            j_ = result
+            break
+    assert j_ != None
+    benchmarks[benchmark][mode] = j_
 
-for benchname, benchspec in spec['benchmarks'].items():
-    print(f'Running tests for {benchname}', file = sys.stderr)
-    for mitigation_name, mitigation_exe in benchspec.items():
-        print(f'Running for mitigation {mitigation_name}', file = sys.stderr)
-        outpath = f'{args.outdir}/{benchname}_{mitigation_name}.json'
-        maybe_run_benchmark(mitigation_exe, outpath)
-        with open(outpath, 'r') as f:
-            print(f'Parsing {outpath}', file = sys.stderr)
-            j = json.load(f)
-        # process json
-        for d in j['benchmarks']:
-            benches[benchname][mitigation_name][d['name']].append(d['cpu_time'])
+# assemble results into table
+data = {
+    'benchmark': [],
+    'overhead': [],
+    'mitigation': [],
+}
 
-data = collections.defaultdict(list)
+geomean_in = defaultdict(list)
 
-geomean_in = collections.defaultdict(list)
+def mitigation_displayname(mitigation):
+    return mitigation.split('_', maxsplit = 1)[-1]
 
-for plotspec in spec['plots']:
-    benchname = plotspec['name']
+def benchmark_displayname(lib, name, size):
+    return f'{lib}\n{name}\n({size})'
 
-    # get json results
-    ref = None
-    for mitigation in spec['mitigations']:
-        mitname = mitigation['name']
-        mitdisplay = mitigation['displayname']
-        j = benches[benchname][mitname]
-        key = plotspec['key']
-        if 'reference' in mitigation:
-            ref = j[f'{key}_mean'][0]
+# TODO: have option for absolute-number style barcharts, not just overhead-style.
+is_absolute = (metric == 'mitigation')
+
+for benchmark in benchmarks:
+    results = benchmarks[benchmark]
+    baseline = results['baseline_none']
+    baseline_metric = baseline[metric_key]
+    for mitigation, result in results.items():
+        if mitigation == 'baseline_none':
             continue
-        means = j[f'{key}_mean']
-        for mean in means:
-            mean_pct = (mean / ref - 1) * 100
-            data['benchmark'].append(plotspec['displayname'])
-            data['mitigation'].append(mitdisplay)
-            data['overhead'].append(mean_pct)
-            geomean_in[mitname].append(mean_pct)
-            
-for mitigation in spec['mitigations']:
-    if 'reference' in mitigation:
-        continue
-    mitname = mitigation['name']
-    mitdisplay = mitigation['displayname']
-    l = geomean_in[mitname]
-    l = [d / 100 + 1 for d in l]
-    assert math.prod(l) > 0
-    geomean = (math.prod(l) ** (1.0 / len(l)) - 1) * 100
-    data['benchmark'].append('Geometric\nmean')
-    data['mitigation'].append(mitdisplay)
-    data['overhead'].append(geomean)
+        mitigation_metric = result[metric_key]
 
-aspect = 1.75
+        if is_absolute:
+            overhead = mitigation_metric
+        else:
+            overhead = (mitigation_metric - baseline_metric) / baseline_metric * 100
+        data['benchmark'].append(benchmark_displayname(*benchmark))
+        data['overhead'].append(overhead)
+        data['mitigation'].append(mitigation_displayname(mitigation))
 
+        geomean_in[mitigation].append(overhead)
+
+scores = {}
+        
+for mitigation, l in geomean_in.items():
+    data['benchmark'].append('geomean')
+    l1 = [x / 100 + 1 for x in l]
+    x = math.prod(l1) ** (1 / len(l))
+    y = (x - 1) * 100
+    data['overhead'].append(y)
+    data['mitigation'].append(mitigation_displayname(mitigation))
+    scores[mitigation_displayname(mitigation)] = y
+
+    data['benchmark'].append('arithmean')
+    data['overhead'].append(sum(l) / len(l))
+    data['mitigation'].append(mitigation_displayname(mitigation))
+
+
+aspect = 2
+    
 df = pandas.DataFrame(data = data)
 g = seaborn.catplot(
     data = df,
@@ -109,48 +116,47 @@ g = seaborn.catplot(
     x = 'benchmark',
     y = 'overhead',
     hue = 'mitigation',
-    errorbar = 'sd',
-    aspect = aspect,
-    height = 5 / aspect,
     legend = None,
+    aspect = aspect,
 )
-g.set_xticklabels(rotation = 45, horizontalalignment = 'center')
+
+g.set_xticklabels(rotation = 45, horizontalalignment = 'center', fontsize = 'small')
+plt.tight_layout()
+
 ax = g.facet_axis(0, 0)
-# ax.figure.tight_layout()
 
-ymin = -6
-ymax = 52.0
+# FIXME: make it proportional, not fixed
+# REVERTME
+# ymax = 1500
+ymax = None
+if ymax is not None:
+    if ax.get_ybound()[1] >= ymax: 
+        ax.set_ybound(upper = ymax)
 
-ax.set_ybound(lower = ymin, upper = ymax)
-
+# fix up bars
 for c in ax.containers:
     labels = []
     for v in c:
-        labels.append(f'{v.get_height():.1f}')
-        v.set_height(min(v.get_height(), ymax))
-    texts = ax.bar_label(c, labels = labels, label_type = 'edge', rotation = 90) #, fontsize = 'small')
-    for text in texts:
-        val = float(text._text)
-        if val < 0:
-            print(f'warning: dropping label for negative bar: {text._text}', file = sys.stderr)
-            text.remove()
-            # text.set_position((0, 0))
-        # if val < 0 or val >= ymax:
-        # text.set(rotation = 0)
+        val = v.get_height()
+        if int(val) == val:
+            s = str(int(val))
+        else:
+            s = f'{val:.1f}'
+        labels.append(s)
+        if ymax is not None:
+            v.set_height(min(v.get_height(), ymax))
+    texts = ax.bar_label(c, labels = labels, label_type = 'edge', rotation = 90, fontsize = 'small')
 
+    
 plt.legend(
-    # fontsize = '7.5',
-    loc = 'upper right',
-    bbox_to_anchor = (1.02, 1.04),
-    labelspacing = 0.3,
-    borderpad = 0.3,
+#    loc = 'upper center',
 )
 
-g.set(
-    ylabel = 'overhead (%)',
-    xlabel = None
-)
+if args.out:
+    plt.savefig(f'{args.out}')
 
-plt.subplots_adjust(top = 0.80, bottom = 0.25, left = 0.1, right = 0.98)
 
-plt.savefig(f'{args.outdir}/plot.pdf')
+# also write file
+with open(args.out + '.txt', 'w') as f:
+    for mitigation, score in scores.items():
+        print(f'{mitigation} {score:.2f}', file = f)
