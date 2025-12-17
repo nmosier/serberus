fbaf2512939cb82663acb412da3d8f9bff01276a
Nicholas Mosier <nmosier@stanford.edu>
2025-12-17 06:36:45 -0800

    update docker scripts

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,10 @@ set -eu
 docker_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 root="$(dirname "$docker_dir")"
 
-docker run --platform=linux/amd64 -v "$root":/serberus -it serberus
+docker run \
+       --platform=linux/amd64 \
+       -v "$root":/serberus \
+       --privileged \
+       -v /dev/cpu:/dev/cpu \
+       -it \
+       serberus
